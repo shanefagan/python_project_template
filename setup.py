515db9c3133cfb9f1ec45060a37b859c8eebf5b5
Shane Fagan <mail@shanefagan.com>
2019-10-30 19:32:58 +0000

    Add a sample for github actions
    - Has a bit of docs to get people started
    - Added a comment about version generation in setup.py
    - Added a little bit to the docs to explain version generation

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ import datetime
 
 from setuptools import setup
 
+# Generate a VERSION file when the package is imported into pipenv
+# changes daily, mainly designed for my laziness when it comes to
+# versioning software
 version = datetime.datetime.now().strftime("%Y%m%d")
 with open("VERSION", "w") as version_file:
     version_file.write(version)
